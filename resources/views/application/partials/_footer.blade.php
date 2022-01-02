02795406194c443c3437ca797d3be2b5bccb4a7d
fadellabanie <fadel.labanie@gmail.com>
2022-01-02 16:40:42 +0200

    layout

---
+++ resources/views/application/partials/_footer.blade.php
@@ -0,0 +1,255 @@
+   <!--footer -->
+   <footer class="main-footer fl-wrap">
+    <!-- footer-header-->
+    <div class="footer-header fl-wrap grad ient-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="subscribe-header">
+                        <h3>Subscribe For a <span>Newsletter</span></h3>
+                        <p>Whant to be notified about new locations ? Just sign up.</p>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="subscribe-widget">
+                        <div class="subcribe-form">
+                            <form id="subscribe">
+                                <input class="enteremail fl-wrap" name="email" id="subscribe-email"
+                                    placeholder="Enter Your Email" spellcheck="false" type="text">
+                                <button type="submit" id="subscribe-button" class="subscribe-button"><i
+                                        class="fal fa-envelope"></i></button>
+                                <label for="subscribe-email" class="subscribe-message"></label>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-header end-->
+    <!--footer-inner-->
+    <div class="footer-inner   fl-wrap">
+        <div class="container">
+            <div class="row">
+                <!-- footer-widget-->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap">
+                        <div class="footer-logo"><a href="index.html"><img
+                                    src="{{asset('application/assets/images/logo.png')}}" alt=""></a></div>
+                        <div class="footer-contacts-widget fl-wrap">
+                            <p>In ut odio libero, at vulputate urna. Nulla tristique mi a massa convallis
+                                cursus. Nulla eu mi magna. Etiam suscipit commodo gravida. </p>
+                            <ul class="footer-contacts fl-wrap no-list-style">
+                                <li><span><i class="fal fa-envelope"></i> Mail :</span><a href="#"
+                                        target="_blank">yourmail@domain.com</a></li>
+                                <li> <span><i class="fal fa-map-marker"></i> Adress :</span><a href="#"
+                                        target="_blank">USA 27TH Brooklyn NY</a></li>
+                                <li><span><i class="fal fa-phone"></i> Phone :</span><a
+                                        href="#">+7(111)123456789</a></li>
+                            </ul>
+                            <div class="footer-social">
+                                <span>Find us on: </span>
+                                <ul class="no-list-style">
+                                    <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-vk"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+                <!-- footer-widget-->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap">
+                        <h3>Our Last News</h3>
+                        <div class="footer-widget-posts fl-wrap">
+                            <ul class="no-list-style">
+                                <li class="clearfix">
+                                    <a href="#" class="widget-posts-img"><img
+                                            src="{{asset('application/assets/images/all/1.jpg')}}" class="respimg"
+                                            alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title="">Vivamus dapibus rutrum</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 21 Mar
+                                            09.05 </span>
+                                    </div>
+                                </li>
+                                <li class="clearfix">
+                                    <a href="#" class="widget-posts-img"><img
+                                            src="{{asset('application/assets/images/all/1.jpg')}}" class="respimg"
+                                            alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title=""> In hac habitasse platea</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 7 Mar
+                                            18.21 </span>
+                                    </div>
+                                </li>
+                                <li class="clearfix">
+                                    <a href="#" class="widget-posts-img"><img
+                                            src="{{asset('application/assets/images/all/1.jpg')}}" class="respimg"
+                                            alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title="">Tortor tempor in porta</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 7 Mar
+                                            16.42 </span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a href="blog.html" class="footer-link"><i class="fal fa-long-arrow-left"></i> Read
+                                all</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+                <!-- footer-widget  -->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap ">
+                        <h3>Our Twitter</h3>
+                        <div class="twitter-holder fl-wrap scrollbar-inner2" data-simplebar
+                            data-simplebar-auto-hide="false" data-simplebar-direction='rtl'>
+                            <div id="footer-twiit"></div>
+                        </div>
+                        <a href="#" class="footer-link twitter-link" target="_blank"><i
+                                class="fal fa-long-arrow-left"></i> Follow us</a>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+            </div>
+        </div>
+        <!-- footer bg-->
+        <div class="footer-bg" data-ran="4"></div>
+        <div class="footer-wave">
+            <svg viewbox="0 0 100 25">
+                <path fill="#fff" d="M0 30 V12 Q30 17 55 12 T100 11 V30z" />
+            </svg>
+        </div>
+        <!-- footer bg  end-->
+    </div>
+    <!--footer-inner end -->
+    <!--sub-footer-->
+    <div class="sub-footer  fl-wrap">
+        <div class="container">
+            <div class="copyright"> &#169; Townhub 2019 . All rights reserved.</div>
+            <div class="subfooter-nav">
+                <ul class="no-list-style">
+                    <li><a href="#">Terms of </a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+            <div class="lang-wrap">
+                <div class="show-lang"><span><i class="fal fa-globe-europe"></i><strong>En</strong></span><i
+                        class="fa fa-caret-down arrlan"></i></div>
+                <ul class="lang-tooltip lang-action no-list-style">
+                    <li><a href="#" class="current-lan" data-lantext="En">English</a></li>
+                    <li><a href="#" data-lantext="Fr">Franais</a></li>
+                    <li><a href="#" data-lantext="Es">Espaol</a></li>
+                    <li><a href="#" data-lantext="De">Deutsch</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--sub-footer end -->
+</footer>
+<!--footer end -->
+<!--map-modal -->
+<div class="map-modal-wrap">
+    <div class="map-modal-wrap-overlay"></div>
+    <div class="map-modal-item">
+        <div class="map-modal-container fl-wrap">
+            <div class="map-modal fl-wrap">
+                <div id="singleMap" data-latitude="40.7" data-longitude="-73.1"></div>
+            </div>
+            <h3><span>Location for : </span><a href="#">Listing Title</a></h3>
+            <div class="map-modal-close"><i class="fal fa-times"></i></div>
+        </div>
+    </div>
+</div>
+<!--map-modal end -->
+<!--register form -->
+<div class="main-register-wrap modal">
+    <div class="reg-overlay"></div>
+    <div class="main-register-holder tabs-act">
+        <div class="main-register fl-wrap  modal_main">
+            <div class="main-register_title">Welcome to <span><strong>Town</strong>Hub<strong>.</strong></span>
+            </div>
+            <div class="close-reg"><i class="fal fa-times"></i></div>
+            <ul class="tabs-menu fl-wrap no-list-style">
+                <li class="current"><a href="#tab-1"><i class="fal fa-sign-in-alt"></i> Login</a></li>
+                <li><a href="#tab-2"><i class="fal fa-user-plus"></i> Register</a></li>
+            </ul>
+            <!--tabs -->
+            <div class="tabs-container">
+                <div class="tab">
+                    <!--tab -->
+                    <div id="tab-1" class="tab-content first-tab">
+                        <div class="custom-form">
+                            <form method="post" name="registerform">
+                                <label>Username or Email Address <span>*</span> </label>
+                                <input name="email" type="text" onClick="this.select()" value="">
+                                <label>Password <span>*</span> </label>
+                                <input name="password" type="password" onClick="this.select()" value="">
+                                <button type="submit" class="btn float-btn color2-bg"> Log In <i
+                                        class="fas fa-caret-right"></i></button>
+                                <div class="clearfix"></div>
+                                <div class="filter-tags">
+                                    <input id="check-a3" type="checkbox" name="check">
+                                    <label for="check-a3">Remember me</label>
+                                </div>
+                            </form>
+                            <div class="lost_password">
+                                <a href="#">Lost Your Password?</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!--tab end -->
+                    <!--tab -->
+                    <div class="tab">
+                        <div id="tab-2" class="tab-content">
+                            <div class="custom-form">
+                                <form method="post" name="registerform" class="main-register-form"
+                                    id="main-register-form2">
+                                    <label>Full Name <span>*</span> </label>
+                                    <input name="name" type="text" onClick="this.select()" value="">
+                                    <label>Email Address <span>*</span></label>
+                                    <input name="email" type="text" onClick="this.select()" value="">
+                                    <label>Password <span>*</span></label>
+                                    <input name="password" type="password" onClick="this.select()" value="">
+                                    <div class="filter-tags ft-list">
+                                        <input id="check-a2" type="checkbox" name="check">
+                                        <label for="check-a2">I agree to the <a href="#">Privacy
+                                                Policy</a></label>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="filter-tags ft-list">
+                                        <input id="check-a" type="checkbox" name="check">
+                                        <label for="check-a">I agree to the <a href="#">Terms and
+                                                Conditions</a></label>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <button type="submit" class="btn float-btn color2-bg"> Register <i
+                                            class="fas fa-caret-right"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!--tab end -->
+                </div>
+                <!--tabs end -->
+                <div class="log-separator fl-wrap"><span>or</span></div>
+                <div class="soc-log fl-wrap">
+                    <p>For faster login or register your social account.</p>
+                    <a href="#" class="facebook-log"> Facebook</a>
+                </div>
+                <div class="wave-bg">
+                    <div class='wave -one'></div>
+                    <div class='wave -two'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--register form end -->
---
+++ resources/views/application/partials/_js.blade.php
@@ -0,0 +1,7 @@
+<script src="{{asset('application/assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('application/assets/js/plugins.js')}}"></script>
+<script src="{{asset('application/assets/js/scripts.js')}}"></script>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE&language=ar&libraries=places&callback=initAutocomplete">
+</script>
+<script src="{{asset('application/assets/js/map-single.js')}}"></script>
